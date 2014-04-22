1d2cce71267ce30a520206d469fa729d738a4631
Gus Cavanaugh <gustafcavanaugh@gmail.com>
2014-04-22 19:57:06 -0400

    created author, reservation, user models

--- library/config/routes.rb
+++ library/config/routes.rb
@@ -1,4 +1,10 @@
 Library::Application.routes.draw do
+  resources :authors
+
+  resources :reservations
+
+  resources :users
+
   resources :books
 
   # The priority is based upon order of creation: first created -> highest priority.
