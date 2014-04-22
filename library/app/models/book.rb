e90ecfe3adaf62918299f1f7ec1e774131c8292b
Gus Cavanaugh <gustafcavanaugh@gmail.com>
2014-04-22 19:38:58 -0400

    created book model

---
+++ library/app/models/book.rb
@@ -0,0 +1,10 @@
+class Book < ActiveRecord::Base
+  GENRE = [sci-fi, thriller, romance, classic, poetry, children]
+  validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, presence: true
+  validates :abstract length: { minimum: 15 }
+  validates :pages, :total_in_library, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :genre, inclusion: { in: GENRE }
+ 
+  
+  
+end
