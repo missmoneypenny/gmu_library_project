14ca255bf2db8ec5483aaede02274d1c8dcc583c
Gus Cavanaugh <gustafcavanaugh@gmail.com>
2014-05-02 22:40:08 -0400

    haven't fixed anything

--- library/app/controllers/users_controller.rb
+++ library/app/controllers/users_controller.rb
@@ -31,7 +31,21 @@ class UsersController < ApplicationController
   end
  
   def index
-    @users = User.order(:name)
+    #@users = User.order(:name)
+  end
+  
+  def new
+    @user = @book.users.new
+  end
+
+  def show
+    @user = User.new
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 
 end
