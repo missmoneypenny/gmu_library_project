6b5573b34a3d7950974e7588f85d6ddc5550f3db
Gus Cavanaugh <gustafcavanaugh@gmail.com>
2014-05-20 19:03:35 -0400

    removed price

--- library/db/schema.rb
+++ library/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520223514) do
+ActiveRecord::Schema.define(version: 20140520230151) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
