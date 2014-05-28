2552c890686d2024aa85aaea2a97c4f1ec09f499
Gus Cavanaugh <gustafcavanaugh@gmail.com>
2014-05-27 21:30:26 -0400

    fingers crossed

--- library/app/models/book.rb
+++ library/app/models/book.rb
@@ -1,10 +1,13 @@
 class Book < ActiveRecord::Base
-  GENRE = ['sci-fi', 'thriller', 'romance', 'classic','poetry', 'children']
+  GENRE = ['science-fiction', 'thriller', 'romance', 'classic','poetry', 'Children']
   validates :isbn, :title, :abstract, :pages, :genre, :published_on, presence: true
   validates :abstract, length: { minimum: 15 }
   validates :pages, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :genre, inclusion: { in: GENRE }
  has_many :reservations
+ 	def self.search(search)
+		where('title LIKE ? OR isbn LIKE ? OR author LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
+	end
   
 end
 
--- library/app/models/reservation.rb
+++ library/app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ActiveRecord::Base
 belongs_to :users
 belongs_to :books
+	def self.search(book_id,user_id)
+	where('book_id = ? AND user_id = ?', "#{book_id}",
+	"#{user_id}").exists?
+	end
 end
